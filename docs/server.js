c75e7f4c3b97152f0f8a8cac580fa6e0e6330ff9
Alicia Sykes <sykes.alicia@gmail.com>
2019-12-31 19:41:39 +0000

    Removed Docsify, I don't think it's really necissary

--- docs/server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
--- docs/src/main.js
+++
@@ -1,8 +0,0 @@
-import './styles/voronoi-styles.scss'
-import D3Voronoi from './scripts/d3-voronoi.ts'
-import SmoothScrolling from './scripts/page-actions.js'
-
-const voronoi = new D3Voronoi();
-
-// I will go to hell for this next line
-Window.finishMdRender = ()=> SmoothScrolling(); 
