e888b0aa8a4d30f34903bc38611928ccd68d0c0e
Jivan Dandapat <jivandandapat@gmail.com>
2022-10-12 19:31:01 +0530

    Added a c program for contribution

---
+++ check.py
@@ -0,0 +1,10 @@
+GeneratorModel for yakindu::python {
+
+	statechart LightSwitch {
+
+		feature Outlet {
+			targetProject = "com.yakindu.examples.python.lightswitch"
+			targetFolder = "src-gen"
+		}
+	}
+}
