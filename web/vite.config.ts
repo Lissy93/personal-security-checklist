f65875e7171dba59ae8fb59fd3a4ae0fab3c328e
Alicia Sykes <alicia@omg.lol>
2024-01-31 21:18:13 +0000

    Creates web

---
+++ web/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, type UserConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig((): UserConfig => {
+  return {
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    server: {
+      headers: {
+        "Cache-Control": "public, max-age=0",
+      },
+    },
+    preview: {
+      headers: {
+        "Cache-Control": "public, max-age=600",
+      },
+    },
+  };
+});
